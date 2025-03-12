5e26b412b586bb75c7b24ccaec24c57ca693ec60
Ivan Andrei <ivamate123@gmail.com>
2025-03-12 14:42:01 +0200

    Added README, template, .gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+# CSES Solutions - Ivan Andrei
---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx2")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ordered_set tree<long long, null_type, less<long long>, rb_tree_tag, tree_order_statistics_node_update>
+#define lsb(x) (x & (-x))
+
+void solve ()
+{
+    
+}
+
+int main ()
+{
+    #ifdef LOCAL
+        freopen("test.in", "r", stdin);
+        freopen("test.out", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    //cin >> T;
+    T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
